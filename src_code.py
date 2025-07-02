b0280cd8e41840f1e9594a592796d5094599762b
Sarthak-gupta-cpp <sarthakgupta1303@gmail.com>
2025-07-02 23:45:42 +0530

    forward pass done without batches

--- src_code.py
+++ src_code.py
@@ -61,7 +61,7 @@ class Convulution_Layer:
         self.image = image_size
 
         self.weights = (
-            np.random.randn(output_shape, input_shape, kernel * kernel) * 0.01
+            np.random.randn(output_shape, input_shape, kernel, kernel) * 0.01
         )  # (10, 1, 4)
         self.biases = np.random.randn(output_shape)
 
@@ -70,16 +70,21 @@ class Convulution_Layer:
         col = np.zeros((self.kernel, self.kernel))
         cols = []
 
-        for i in range(0, out_shape, self.stride):
-            for j in range(0, out_shape, self.stride):
-                col = (x[i : i + self.kernel, j : j + self.kernel]).flatten()
-                col = self.weights @ col  # (10, 1, 4) # (4, )
-                col = np.sum(col, axis=1) + self.biases
-                cols.append(col)  # col is (10, )
+        for fil in range(self.output_shape):
+            for i in range(0, out_shape, self.stride):
+                for j in range(0, out_shape, self.stride):
+                    col = x[:, i : i + self.kernel, j : j + self.kernel]
+                    print(col.shape)
+                    col = self.weights @ col  # (10, 1, 2, 2) # (1, 2, 2)
+                    print(col.shape)
+                    col = np.sum(col, axis=1) + self.biases
+                    print(col.shape)
+                    cols.append(col)  # col is (10, )
 
         out = np.array(np.column_stack(cols))
+        print(out.shape)
         out = out.reshape(self.output_shape, out_shape, out_shape)
-        print(out)
+        out = ReLu.fn(out)
         fig, axes = plt.subplots(5, 2, figsize=(10, 10))
         for i, ax in enumerate(axes.flat):
             ax.imshow(out[i], cmap="gray")
@@ -88,7 +93,10 @@ class Convulution_Layer:
 
 
 Layert = Convulution_Layer()
-out = Layert.forward(train_x[90].reshape(28, 28))
+out = Layert.forward(train_x[10].reshape(1, 28, 28))
+
+Layerth = Convulution_Layer(input_shape=10, image_size=27)
+out2 = Layerth.forward(out)
 
 
 class NeuralNet:
